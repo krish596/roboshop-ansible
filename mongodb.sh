5b73efdda8473c4dc6d8d4f5f1733e36fd4f09ff
Chimtamreddy <kr7348202@gmail.com>
2024-08-04 15:49:00 +0530

    roboshop-shellscript

--- mongodb.sh
+++ mongodb.sh
@@ -21,10 +21,3 @@
         state: restarted
         enabled: yes
 
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-org -y
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl enable mongod
-systemctl restart mongod
